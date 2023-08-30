6ce59c9ef6113a91c3d4e7605bb94fcc6471662e
Ankur <anx450z@gmail.com>
2023-08-30 17:52:56 +0530

    working with controller with action

--- src/showController.ts
+++ src/showController.ts
@@ -9,9 +9,8 @@ export class RubyControllerCodeLensProvider implements vscode.CodeLensProvider {
   ): Promise<vscode.CodeLens[]> {
     const codeLenses: vscode.CodeLens[] = [];
     const isViewFile = /app[\/\\]views[\/\\][^\/\\]+/.test(document.fileName);
-    console.log("this is view file");
+
     if (!isViewFile) {
-      console.log('not view file');
       return codeLenses;
     }
 
@@ -24,21 +23,34 @@ export class RubyControllerCodeLensProvider implements vscode.CodeLensProvider {
 
       const viewFilePath = document.uri.fsPath;
       const viewRelativePath = path.relative(path.join(workspacePath, "app", "views"), viewFilePath.split('.')[0]);
-      console.log('view file path', viewRelativePath);
       const [controller, action] = this.extractControllerActionFromViewPath(viewRelativePath);
 
       if (controller && action) {
         const controllerFilePath = path.join(workspacePath, "app", "controllers", `${controller}_controller.rb`);
-        
+
         if (fs.existsSync(controllerFilePath)) {
           const codeLensRange = new vscode.Range(0, 0, 0, 0);
+          const controllerFileContent = fs.readFileSync(controllerFilePath, "utf-8");
+          const actionExists = this.controllerActionExists(controllerFileContent, action);
+
           const codeLens = new vscode.CodeLens(codeLensRange);
           codeLens.command = {
-            title: `🎮 CONTROLLER: ${controller}#${action}`,
+            title: `🎮 CONTROLLER: ${controller}`,
             command: "vscode.open",
-            arguments: [vscode.Uri.file(controllerFilePath)]
+            arguments: [vscode.Uri.file(controllerFilePath)],
+            tooltip: `NAVIGATE TO CONTROLLER → ${controller}`
           };
           codeLenses.push(codeLens);
+          if (actionExists) {
+            const codeLensAction = new vscode.CodeLens(codeLensRange);
+            codeLensAction.command = {
+              title: `${action}`,
+              command: 'vscode.open',
+              arguments: [vscode.Uri.file(controllerFilePath)],
+              tooltip: `NAVIGATE TO ACTION → ${controller}#${action}`
+            };
+            codeLenses.push(codeLensAction);
+          }
         }
       }
 
@@ -51,13 +63,14 @@ export class RubyControllerCodeLensProvider implements vscode.CodeLensProvider {
 
   private extractControllerActionFromViewPath(viewRelativePath: string): [string, string] | [] {
     // Implement the logic to extract controller and action from the viewRelativePath
-    // For example, convert "api/v1/users/index.json.jbuilder" to ["api/v1/users", "index"]
     const separatedPath =  viewRelativePath.split('/');
-    console.log('separatedPath', separatedPath);
     const action = separatedPath.pop() || '';
-    console.log('action: ' + action);
     const controller = separatedPath.join('/');
-    console.log('controller: ' + controller);
     return [controller, action];
+  }
+
+  private controllerActionExists(controllerFileContent: string, action: string): boolean {
+    // Implement the logic to check if the given action exists in the controller file content
+    return controllerFileContent.includes(`def ${action}`);
   }
 }
